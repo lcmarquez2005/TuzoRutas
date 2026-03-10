9ef7314e528dd3e98424a47d926353398408cc0f
80chucho08 <chucho2004carbajal@gmail.com>
2026-03-09 21:44:44 -0600

    fix: limpieza de pantallas en app.tsx a screens separadas reaciendo el tab navigator

--- App.tsx
+++ App.tsx
@@ -1,82 +1,12 @@
 import React from 'react';
 import './global.css';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text } from 'react-native';
-import { ScreenContent } from '@/screens/ScreenContent'; // Ajusta la ruta
-import RutasScreen from '@/screens/RutasScreen';
-import Button from '@/components/Button';
-
-// icon library from expo (already included with expo)
-import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-
-// 1. Definimos las pantallas (puedes moverlas a archivos separados luego)
-const InicioScreen = () => (
-  <ScreenContent title="Inicio" path="screens/Inicio.tsx">
-    <Text className="mt-6 text-gray-600">Bienvenido a TuzoRutas</Text>
-    <Text className="bg-gray text-gray mt-8 p-4 ">Bienvenido a TuzoRutas</Text>
-  </ScreenContent>
-);
-
-const AjustesScreen = () => (
-  <ScreenContent title="Ajustes" path="screens/Ajustes.tsx">
-    <Text className="mt-4 text-gray-600">Configuración de la app</Text>
-
-
-// Contained button (default)
-    <Button onPress={() => console.log('Pressed')}>Save</Button>
-
-// Outlined variant
-    <Button className='mt-4' variant="outlined" onPress={() => console.log('Pressed')}>Cancel</Button>
-
-    <Button className='mt-4' variant="text" onPress={() => console.log('Pressed')}>Cancel</Button>
-    <Button className='mt-4' color="secondary" onPress={() => console.log('Pressed')}>Cancel</Button>
-    <Button className='mt-4' variant="outlined" disabled onPress={() => console.log('Pressed')}>Cancel</Button>
-
-  </ScreenContent>
-);
-
-const Tab = createBottomTabNavigator();
+import { TabNavigator } from './src/navigation/TabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false, // Oculta el título superior por defecto
-          tabBarStyle: { backgroundColor: '#000000', height: 60 },
-          tabBarActiveTintColor: '#800000', // Un color guinda tipo Tuzobús
-          tabBarInactiveTintColor: 'gray',
-        }}>
-        <Tab.Screen
-          name="Inicio"
-          component={InicioScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="home" size={size} color={color} />
-            ),
-            tabBarLabel: 'Home',
-          }}
-        />
-        <Tab.Screen
-          name="Rutas"
-          component={RutasScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => <MaterialIcons name="map" size={size} color={color} />,
-            tabBarLabel: 'Mapa',
-          }}
-        />
-        <Tab.Screen
-          name="Ajustes"
-          component={AjustesScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="settings" size={size} color={color} />
-            ),
-            tabBarLabel: 'Ajustes',
-          }}
-        />
-      </Tab.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
