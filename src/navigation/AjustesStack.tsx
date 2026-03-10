8f3e01fd4407cdb548336bdaf29b4d3d741fc93f
80chucho08 <chucho2004carbajal@gmail.com>
2026-03-09 21:45:55 -0600

    feat: navegacion para trakeo de rutas usando la navegacion de tipo stack

---
+++ src/navigation/AjustesStack.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Importa tus pantallas
+import AjustesScreen from '../screens/AjustesScreen';
+import TrackRoutes from '../screens/TrackRoutes';
+
+const Stack = createNativeStackNavigator();
+
+export const AjustesStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#800000' }, // Guinda Tuzobús
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' },
+      }}
+    >
+      <Stack.Screen 
+        name="AjustesHome" 
+        component={AjustesScreen} 
+        options={{ title: 'Configuración' }} 
+      />
+      <Stack.Screen 
+        name="Track" 
+        component={TrackRoutes} 
+        options={{ title: 'Trazar Ruta' }} 
+      />
+    </Stack.Navigator>
+  );
+};
